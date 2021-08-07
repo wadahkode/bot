44735a6c06ce1db460170d70273d599d75561418
La Ode Muhammad Al fatih <lamualfa@gmail.com>
2021-08-07 21:32:28 +0700

    chore: migrate to vercel

--- src/app.js
+++ src/app.js
@@ -4,9 +4,7 @@ import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 import redis from 'redis';
 
-// TODO: Handle global errors
-// import logger from './utlis/logger.js';
-
+import logger from './utlis/logger.js';
 import * as poll from './services/poll.js';
 import * as meme from './services/meme.js';
 import * as time from './services/time.js';
@@ -22,6 +20,12 @@ dotenv.config({ path: envPath });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const cache = redis.createClient(String(process.env.REDIS_URL));
+
+// Handle bot error globally
+// Register it first before register any other commands
+bot.catch((err) => {
+  logger.captureException(err);
+});
 
 const commands = [
   meme.register(bot),
@@ -41,13 +45,11 @@ bot.telegram.setMyCommands(commands);
 
 // TODO: Handle command not found
 
-bot.launch();
-
-function terminate(caller) {
-  cache.QUIT();
-  return bot.stop(caller);
+if (process.env.NODE_ENV === 'development') {
+  // We don't need this again in production
+  bot.launch();
+} else {
+  bot.telegram.setWebhook(process.env.VERCEL_URL);
 }
 
-// Enable graceful stop
-process.once('SIGINT', () => terminate('SIGINT'));
-process.once('SIGTERM', () => terminate('SIGTERM'));
+export default bot.webhookCallback('/');
